bd7fc3715c0cf5ac3bae89db6edfc8412fa03cc6
unknown <damilaresani@gmail.com>
2022-05-25 21:34:52 +0100

    Customizes portfolio

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,11 +7,11 @@ const Header = () => {
             <img src={profile} alt="John Doe" />
 
             <div className='header__content'>
-                <h1>Hi, I'm John Doe</h1>
+                <h1>Hi, I'm Papilo</h1>
                 <p>Blockchain Developer</p>
-                <button className='button'>Get In Touch</button>
+                <a href="mailto:damilaresani@gmail.com" className='button'>Get In Touch</a>
             </div>
-        </section>
+        </section> 
     );
 }
 
